31fc33d7ee29847fc526d3ef721f09b2c71d8a63
ManojkumarT555 <manojkumart2004@gmail.com>
2026-06-22 22:12:14 +0530

    Validated API for reqres.in

--- tests/reqresapi.test.ts
+++ tests/reqresapi.test.ts
@@ -19,4 +19,71 @@ test.describe('ReqresAPI', () => {
         const body = await response.json();
         console.log(body);
     });
+
+    test('Validating POST API', async ({ request }) => {
+        const response = await request.post(
+            `${baseUrl}/users`,
+            {
+            headers: {
+                'x-api-key': apiKey
+            },
+            data: {
+                name: 'Kumar',
+                job: 'QA Engineer'
+            }
+            }
+        );
+
+        expect(response.status()).toBe(201);
+
+        const body = await response.json();
+
+        console.log(body);
+
+        expect(body.name).toBe('Kumar');
+        expect(body.job).toBe('QA Engineer');
+        expect(body.id).toBeTruthy();
+        expect(body.createdAt).toBeTruthy();
+
+    });
+
+    test('Update User', async ({ request }) => {
+
+        const response = await request.put(
+            `${baseUrl}/users/2`,
+            {
+            headers: {
+                'x-api-key': apiKey
+            },
+            data: {
+                name: 'KumarVel',
+                job: 'Senior QA'
+            }
+            }
+        );
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        expect(body.name).toBe('KumarVel');
+        expect(body.job).toBe('Senior QA');
+        expect(body.updatedAt).toBeTruthy();
+
+    });
+
+    test('Delete User', async ({ request }) => {
+
+        const response = await request.delete(
+            `${baseUrl}/users/2`,
+            {
+            headers: {
+                'x-api-key': apiKey
+            }
+            }
+        );
+
+        expect(response.status()).toBe(204);
+
+    });
 });
